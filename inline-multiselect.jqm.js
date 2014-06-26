ff4a1652e39e47bf45412f240f7757499f9ca1d1
jlh752 <jlh752@uowmail.edu.au>
2014-06-26 15:50:33 +1000

    build checkbox system based on multiselect

--- inline-multiselect.jqm.js
+++ inline-multiselect.jqm.js
@@ -5,7 +5,33 @@
 	$.widget("mobile.inlinemultiselect", $.mobile.widget, {
 		_create: function () {
 			var $elem = this.element;
+			$elem.selectmenu("destroy");
+
+			var holder = $("<div class='inline-multiselect'><p></p></div>");
+			var fs = $("<fieldset data-role='controlgroup'></fieldset>");
+
+			var a = $('.inline-multiselect').length, c = 0;
+			var options = {};
+			$elem.find("option").each(function (x, y) {
+				var uniq = "inlinems-" + a + "-" + c;
+				$(y).attr("data-multiselect", uniq);
+				var check = $("<input type='checkbox' name='" + uniq + "' id='" + uniq + "' />");
+				if($(y).is(":selected"))
+					check.prop('checked', true);
+				var label = $("<label for='" + uniq + "' >" + $(y).text() + "</label>");
+				fs.append(check).append(label);
+
+				c++;
+			});
+
+			holder.find("p").append(fs);
+
+			$elem.after(holder);
+			holder.trigger('create');
+
 			
+			$elem.hide();
+			$('label[for=' + $elem.attr('id') + ']').hide();
 		}
 	});
 })(jQuery);
